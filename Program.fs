509f735005aa84ce40d7832f449719e98e5b1c91
fanGam <kosyurakosyura@gmail.com>
2023-05-23 11:26:53 +0500

    Добавьте файлы проекта.

---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let Rand = new Random()
+
+let n = 15
+
+let baza = [
+    for i in 0..n-1 do
+        yield [Rand.Next(1, 100000) ; Rand.Next(1, 100000)] ]
+
+for i in 0..n-1 do
+    printfn $"{i+1} pair: {baza[i]}"
